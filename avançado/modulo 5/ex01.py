aa3e857175bd2598a56158fe770e83dd077adf1a
Mariane Andrade <mariane.andrade010@gmail.com>
2025-10-30 20:32:30 -0300

    Update ex01

--- avançado/modulo 5/ex01.py
+++ avançado/modulo 5/ex01.py
@@ -1,3 +1,7 @@
-# Meu primeiro programa!
+# Exercício Módulo 1 - Operações, Variáveis e Input.
 
-print('Olá, Mundo!')
+print("-"*30)
+cod = input('Digite o código: ').upper()
+
+print('Bebida Alcoólica:', 'BAC' in cod)
+print('Bebida Não Alcoólica:', 'BEB' in cod)
