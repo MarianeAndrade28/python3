1d7220e274b78a4a11109eab3377198802d2ba79
Mariane Andrade <mariane.andrade010@gmail.com>
2025-11-04 18:54:11 -0300

    Update curso em vídeo

--- curso em vídeo/modulo 3 - estruturas compostas/ex075.py
+++
@@ -1,8 +0,0 @@
-# Desafio 075: Desenvolva um programa que leia quatro valores pelo teclado e guarde-os em uma tupla. No final, mostre:
-# a) Quantas vezes apareceu o valor 9; b) Em que posição foi digitado o primeiro valor 3; c) Quais foram os números pares.
-
-num = (int(input("Digite um número: ")),
-       int(input("Digite outro número: ")),
-       int(input("Digite mais um número: ")),
-       int(input("Digite o último número: ")))
-print(f"Você digitou os valores: {num}")
