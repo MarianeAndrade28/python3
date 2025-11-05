dbeb0ead565eefe85ab9f5dcf0f365efeb015748
Mariane Andrade <mariane.andrade010@gmail.com>
2025-11-05 11:09:20 -0300

    Update modulo 7

---
+++ avançado/modulo 7/ex01.py
@@ -0,0 +1,18 @@
+vendFunc1 = 1000
+vendFunc2 = 770
+vendFunc3 = 2700
+
+if vendFunc1 >= 1000:
+    print(f'O funcionário 1 recebeu {vendFunc1 * 0.1:.0f} de bônus')
+if vendFunc2 >= 1000:
+    bônus = vendFunc2 * 0.1
+else:
+    bônus = 0
+print(f'O funcionário 2 recebeu {bônus:.0f} de bônus')
+
+if vendFunc3 >= 1000:
+    bônus = vendFunc3 * 0.1
+else:
+    bônus = 0
+print(f'O funcionário 3 ganhou {bônus:.0f} de bônus')
+
---
+++ avançado/modulo 7/ex02.py
@@ -0,0 +1,24 @@
+categoria = input('Categoria: ').lower()
+produto = input('Nome do Produto: ').upper()
+quantidade = input('Quantidade atual do produto: ')
+
+if categoria and produto and quantidade:
+    quantidade = int(quantidade)
+    if categoria == 'alimento':
+        if quantidade < 50:
+            print(f'Solicitar {produto} à equipe de compras, temos apenas {quantidade} unidades em estoque')
+
+    elif categoria == 'bebida':
+        if quantidade < 75:
+            print(f'Solicitar {produto} à equipe de compras, temos apenas {quantidade} unidades em estoque')
+
+    else:
+        if quantidade < 30:
+            print(f'Solicitar {produto} à equipe de compras, temos apenas {quantidade} unidades em estoque')
+
+    if categoria == 'limpeza':
+        if quantidade < 30:
+            print(f'Solicitar {produto} à equipe de compras, temos apenas {quantidade} unidades em estoque')
+
+else:
+    print('Por favor, preencha todos os campos')
