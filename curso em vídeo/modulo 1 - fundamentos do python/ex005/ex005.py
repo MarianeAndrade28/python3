f283f726f4f650842cc2c7637f99d78994c618fa
Mariane Andrade <mariane.andrade010@gmail.com>
2025-12-24 11:46:03 -0300

    Update ex005.py

--- curso em vídeo/modulo 1 - fundamentos do python/ex005/ex005.py
+++ curso em vídeo/modulo 1 - fundamentos do python/ex005/ex005.py
@@ -1,5 +1,3 @@
-# Desafio 005: Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.
-
 numero = int(input("Digite um número: "))
 sucessor = numero + 1
 antecessor = numero - 1
