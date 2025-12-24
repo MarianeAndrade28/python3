f7e80b9e50e61c43e28839b1fdff524a821b52a6
Mariane Andrade <mariane.andrade010@gmail.com>
2025-12-24 11:45:31 -0300

    Update ex004.py

--- curso em vídeo/modulo 1 - fundamentos do python/ex004/ex004.py
+++ curso em vídeo/modulo 1 - fundamentos do python/ex004/ex004.py
@@ -1,5 +1,3 @@
-# Desafio 004: Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ela.
-
 leia = input("Digite algo: ")
 print("O tipo primitivo desse valor é:", type(leia))
 print("Só tem espaços?", leia.isspace())
